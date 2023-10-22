00c53ed65f5babaa113710c7b62cab96cf53c329
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-22 10:28:07 -0500

    up to date

---
+++ client/app/auth/SignInForm.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup
+  .object()
+  .shape({
+    email: yup.string().required().email(),
+    password: yup.string().required().min(8),
+  })
+  .required();
+const SignInForm = () => {
+  const;
+};
+
+export default SignInForm;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,51 +1,66 @@
-import Chat from "./components/Chat"
-import type {Message} from "./types"
-import { cookies } from 'next/headers'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import Chat from './components/Chat';
+import type { Database } from './types/database.types';
+import { cookies } from 'next/headers';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 
 export default async function Home() {
-	const cookieStore = cookies()
-	const supabase = createServerComponentClient({ cookies: () => cookieStore })
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient<Database>({
+    cookies: () => cookieStore,
+  });
 
-	let { data: chats, error } = await supabase
-		.from('chats')
-		.select()
-		.eq('id', '1dfb9e26-4711-4ba3-98ca-da893894745d');
+  let { data: chats, error } = await supabase
+    .from('chats')
+    .select()
+    .eq('chat_id', '1dfb9e26-4711-4ba3-98ca-da893894745d');
 
-	if (error) console.error(error);
-	
-	const [chat] = chats;
-	const {id: chat_id, user_1_id, user_2_id} = chat;
+  if (error) console.error(error);
 
-	const {data: messages, error: messageError} = await supabase
-		.from('messages')
-		.select()
-		.eq('chat_id', chat_id);
+  if (!chats)
+    return (
+      <>
+        <p>Please create a chat to get started!</p>
+      </>
+    );
 
-	if (messageError) console.error(messageError);
+  const [chat] = chats;
+  const { chat_id, chat_name } = chat;
 
-	const {data: userData, error: profileError} = await supabase
-		.from('profiles')
-		.select()
-		.eq('id', user_1_id)
-		.single();
+  const { data: messages, error: messageError } = await supabase
+    .from('messages')
+    .select()
+    .eq('chat_id', chat_id);
 
-	if (profileError) console.error(profileError);
-	
-	return (
-		<main className="flex flex-col h-[100vh] bg-gray-500 w-full gap-6">
-			{/* Header */}
-			<section
-				className="w-full flex flex-row gap-2 py-8 px-8 border-b border-b-gray-200"
-			>
-				<p>Back</p>
-				<p>Avatar</p>
-				<p>Name</p>
-				<p className="ml-auto">Settings</p>
-			</section>
+  if (messageError) console.error(messageError);
 
-			{/* Chat */}
-			<Chat messagesData={messages} user={userData} />
-		</main>
-	)
+  if (!messages)
+    return (
+      <>
+        <p>
+          There are no messages in this chat yet. Send a message to get started!
+        </p>
+      </>
+    );
+
+  const { data: chat_users, error: chatUsersError } = await supabase
+    .from('chat_users')
+    .select()
+    .eq('chat_id', chat_id);
+
+  console.log(chat_users);
+
+  if (chatUsersError) console.error(chatUsersError);
+
+  return (
+    <>
+      {/* Chat */}
+
+      {/* <pre>{JSON.stringify(messages, null, 2)}</pre>
+
+      {chat_users?.map((chat_user) => {
+        return <p>{chat_user.user_id}</p>;
+      })} */}
+      <Chat messagesData={messages} />
+    </>
+  );
 }
