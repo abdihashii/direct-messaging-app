f195a0470ddf1fedb2c452e67348922614f2bd9c
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-23 15:05:48 -0500

    New chat group page and allows you to create a new chat group

---
+++ client/app/chats/new/NewChatForm.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import Link from 'next/link';
+
+const schema = yup
+  .object()
+  .shape({
+    chatName: yup.string().required(),
+  })
+  .required();
+
+const NewChatForm = () => {
+  const supabase = createClientComponentClient();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleCreateChatRoom = async (formData: { chatName: string }) => {
+    try {
+      const { data, error } = await supabase.from('chats').insert([
+        {
+          chat_name: formData.chatName,
+        },
+      ]);
+
+      if (error) {
+        throw error;
+      }
+
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onSubmit = (data: { chatName: string }) => {
+    handleCreateChatRoom(data);
+  };
+
+  return (
+    <form
+      className="flex flex-col gap-4 w-full"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <section className="flex flex-col gap-2">
+        <Label htmlFor="chat-name">Chat Name</Label>
+        <Input type="text" id="chat-name" {...register('chatName')} />
+        <p className="text-red-500">{errors.chatName?.message}</p>
+      </section>
+
+      <Button className="w-full" type="submit">
+        Create Chat
+      </Button>
+
+      <Link href="/chats" className="w-full">
+        <Button type="button" variant={'outline'} className="w-full">
+          Back to Chats
+        </Button>
+      </Link>
+    </form>
+  );
+};
+
+export default NewChatForm;
