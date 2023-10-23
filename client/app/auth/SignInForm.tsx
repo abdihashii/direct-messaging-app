07f8a3f1eb1bfbe0945f855f9391d5c0a8390389
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-23 13:09:04 -0500

    basic design of sign in form

--- client/app/auth/SignInForm.tsx
+++ client/app/auth/SignInForm.tsx
@@ -3,6 +3,9 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 const schema = yup
   .object()
@@ -16,10 +19,22 @@ const SignInForm = () => {
   const {} = useForm({});
 
   return (
-    <form>
-      <input type="email" name="email" />
-      <input type="password" name="password" />
-      <button type="submit">Sign In</button>
+    <form className="flex flex-col gap-8 w-full">
+      <section className="flex flex-col gap-2">
+        <Label htmlFor="email">Email</Label>
+        <Input type="email" name="email" id="email" />
+      </section>
+
+      <section className="flex flex-col gap-2">
+        <Label htmlFor="password">Password</Label>
+        <Input type="password" name="password" id="password" />
+      </section>
+
+      <section>
+        <Button type="submit" className="w-full">
+          Sign In
+        </Button>
+      </section>
     </form>
   );
 };
--- client/app/auth/sign-in/page.tsx
+++ client/app/auth/sign-in/page.tsx
@@ -2,8 +2,8 @@ import SignInForm from '../SignInForm';
 
 export default function SignInPage() {
   return (
-    <main>
-      <h1>Sign In</h1>
+    <main className="h-screen justify-center gap-8 flex flex-col items-center mx-auto w-10/12">
+      <h1 className="text-4xl font-semibold">Welcome Back!</h1>
 
       <SignInForm />
     </main>
