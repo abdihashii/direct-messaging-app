e5829a5ff50e38881d6de2efab32e52516dbfdb5
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-23 15:05:14 -0500

    getUserName util function

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,6 +1,27 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function getUserName(userId: string | undefined | null) {
+  if (!userId) return '';
+
+  const supabase = createClientComponentClient();
+
+  try {
+    const { data, error } = await supabase
+      .from('profiles')
+      .select('user_name')
+      .eq('user_id', userId)
+      .single();
+
+    if (error) throw error;
+
+    return data.user_name;
+  } catch (error) {
+    return '';
+  }
 }
