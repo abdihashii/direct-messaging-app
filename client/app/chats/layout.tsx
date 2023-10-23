110dbe88792ccf42f541fa7c7f249bc0b6d8f011
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-23 15:05:03 -0500

    Shows avatar in chats layout

--- client/app/chats/layout.tsx
+++ client/app/chats/layout.tsx
@@ -1,15 +1,25 @@
-export default function ChatLayout({
+import { getUser } from '@/lib/supabaseServerClient';
+import { getUserName } from '@/lib/utils';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+export default async function ChatsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await getUser();
+  const userName = await getUserName(user?.id);
+
   return (
-    <main className="flex flex-col h-[100vh] bg-gray-500 w-full gap-6">
-      {/* Header */}
-      <section className="w-full flex flex-row gap-2 py-8 px-8 border-b border-b-gray-200">
-        <p>Back</p>
-        <p>Avatar</p>
-        <p>Name</p>
+    <main className="flex flex-col h-[100vh]w-full gap-6">
+      <section className="items-center w-full flex flex-row gap-2 py-8 px-8 border-b-2 border-b-gray-500">
+        <div className="flex flex-row items-center gap-2">
+          <Avatar>
+            <AvatarFallback>TU</AvatarFallback>
+          </Avatar>
+          <p className="text-sm">{userName}</p>
+        </div>
+
         <p className="ml-auto">Settings</p>
       </section>
 
