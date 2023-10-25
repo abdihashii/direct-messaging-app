22445e2f789338f90017bb2143d27a8aafdc430c
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-25 15:53:20 -0500

    Can send a chat to the group

--- client/app/chats/[chat_id]/ChatActions.tsx
+++ client/app/chats/[chat_id]/ChatActions.tsx
@@ -4,6 +4,9 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { Input } from '@/components/ui/input';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useState } from 'react';
+import { Loader2, SendHorizonal } from 'lucide-react';
 
 const schema = yup
   .object()
@@ -12,12 +15,49 @@ const schema = yup
   })
   .required();
 
-const ChatActions = () => {
+const ChatActions = ({
+  chatId,
+  senderId,
+}: {
+  chatId: string;
+  senderId: string;
+}) => {
   const { register, handleSubmit } = useForm({
     resolver: yupResolver(schema),
   });
+  const supabase = createClientComponentClient();
+  const [isLoading, setIsLoading] = useState(false);
 
-  const onSubmit = (data: { content: string }) => console.log(data);
+  const onSubmit = async (data: { content: string }) => {
+    setIsLoading(true);
+
+    if (!data.content) {
+      setIsLoading(false);
+      alert('Please enter a message');
+      return;
+    }
+
+    try {
+      const { data: messages, error } = await supabase
+        .from('messages')
+        .insert([
+          {
+            chat_id: chatId,
+            sender_id: senderId,
+            content: data.content,
+          },
+        ])
+        .select();
+
+      if (error) throw new Error(error.message);
+
+      alert(JSON.stringify(messages, null, 2));
+    } catch (error: any) {
+      alert(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div className="mt-auto w-full py-6 px-5 border-t-2 border-t-gray-500">
@@ -26,16 +66,20 @@ const ChatActions = () => {
         onSubmit={handleSubmit(onSubmit)}
       >
         <Input
-          className="text-black px-4 py-3 w-full"
+          className="text-black px-4 py-3"
           type="text"
           placeholder="Message"
           {...register('content')}
         />
         <button
           type="submit"
-          className="bg-green-600 text-white px-4 rounded ml-auto hover:bg-green-700 transition-colors duration-200"
+          className="bg-green-600 text-white px-4 rounded ml-auto hover:bg-green-700 transition-colors duration-200 w-fit"
         >
-          Send
+          {isLoading ? (
+            <Loader2 className="animate-spin" size={20} />
+          ) : (
+            <SendHorizonal size={20} />
+          )}
         </button>
       </form>
     </div>
