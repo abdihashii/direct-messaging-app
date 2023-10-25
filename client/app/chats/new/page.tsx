77408ca9a6b9285ab6bc51dec208a842836982a5
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-25 15:52:47 -0500

    fixes new chat form look

--- client/app/chats/new/page.tsx
+++ client/app/chats/new/page.tsx
@@ -2,7 +2,7 @@ import NewChatForm from './NewChatForm';
 
 export default async function NewChatPage() {
   return (
-    <section className="mt-auto flex flex-col gap-8 px-8">
+    <section className="h-screen justify-center flex flex-col gap-8 px-8">
       <h1 className="text-2xl font-semibold">Create a new chat room</h1>
 
       <NewChatForm />
