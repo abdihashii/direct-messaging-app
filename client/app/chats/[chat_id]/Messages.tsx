f28a021d89dfa646051abb37b334b8f7996e6098
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-25 21:01:14 -0500

    Add message state and scroll to bottom

--- client/app/chats/[chat_id]/Messages.tsx
+++ client/app/chats/[chat_id]/Messages.tsx
@@ -2,36 +2,60 @@
 
 import { getUserName } from '@/lib/utils';
 import { Message } from '@/types';
+import { useRef, useEffect, useState } from 'react';
 
 const Messages = ({
   messages,
   userId,
+  chatName,
 }: {
   messages: Message[];
   userId: string;
+  chatName: string;
 }) => {
-  return messages?.map(async (m) => {
-    const userName = await getUserName(m.sender_id);
-    const iAmTheSender = userId === m.sender_id;
+  const chatContainerRef = useRef<HTMLDivElement>(null);
+  const [messagesState, setMessagesState] = useState<Message[]>(messages);
 
-    return (
-      <li
-        className={`max-w-[50%] p-2 border rounded-md border-gray-500 flex flex-col gap-2 ${
-          iAmTheSender ? 'items-end ml-auto' : 'items-start justify-start'
-        }`}
-        key={m.message_id}
-      >
-        <p
-          className={`text-sm ${
-            iAmTheSender ? 'text-green-700' : 'text-blue-700'
-          }`}
-        >
-          {userName}
-        </p>
-        <p>{m.content}</p>
-      </li>
-    );
-  });
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  }, [messagesState]);
+
+  return (
+    <div
+      ref={chatContainerRef}
+      className="flex flex-col items-center gap-8 overflow-y-scroll scroll-smooth p-5"
+    >
+      <h1 className="text-2xl font-semibold">{chatName}</h1>
+
+      <ul className="flex w-full flex-col gap-4">
+        {messagesState?.map(async (m) => {
+          const userName = await getUserName(m.sender_id);
+          const iAmTheSender = userId === m.sender_id;
+
+          return (
+            <li
+              className={`flex max-w-[50%] flex-col gap-2 rounded-md border border-gray-500 p-2 ${
+                iAmTheSender ? 'ml-auto items-end' : 'items-start justify-start'
+              }`}
+              key={m.message_id}
+            >
+              <p
+                className={`text-sm ${
+                  iAmTheSender ? 'text-green-700' : 'text-blue-700'
+                }`}
+              >
+                {userName}
+              </p>
+              <p>{m.content}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Messages;
--- client/app/chats/[chat_id]/page.tsx
+++ client/app/chats/[chat_id]/page.tsx
@@ -30,21 +30,19 @@ export default async function ChatPage({
     const error = messagesError || chatError;
 
     return (
-      <section className="flex flex-col gap-4 px-8 items-center">
-        <p className="text-xl text-center">{error?.message}</p>
+      <section className="flex flex-col items-center gap-4 px-8">
+        <p className="text-center text-xl">{error?.message}</p>
       </section>
     );
   }
 
   return (
-    <section className="flex flex-col h-full">
-      <div className="flex flex-col gap-8 p-5 items-center overflow-y-scroll">
-        <h1 className="text-2xl font-semibold">{chat.chat_name}</h1>
-
-        <ul className="w-full flex flex-col gap-4">
-          <Messages messages={messages} userId={user!.id} />
-        </ul>
-      </div>
+    <section className="flex h-full flex-col">
+      <Messages
+        messages={messages}
+        userId={user!.id}
+        chatName={chat.chat_name as string}
+      />
 
       <ChatActions chatId={params.chat_id} senderId={user!.id} />
     </section>
