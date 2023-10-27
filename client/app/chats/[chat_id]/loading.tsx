1cee180f9e5823364cd2034e7bdfcebd6d046879
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-26 20:41:26 -0500

    basic loading pages for chats and chat_id

---
+++ client/app/chats/[chat_id]/loading.tsx
@@ -0,0 +1,7 @@
+export default function LoadingChat() {
+  return (
+    <section className="flex flex-col items-center gap-4 px-8">
+      <p className="text-center text-xl">Loading...</p>
+    </section>
+  );
+}
---
+++ client/app/chats/loading.tsx
@@ -0,0 +1,7 @@
+export default function LoadingChatRooms() {
+  return (
+    <section className="flex flex-col items-center gap-4 px-8">
+      <p className="text-center text-xl">Loading Chat Rooms...</p>
+    </section>
+  );
+}
