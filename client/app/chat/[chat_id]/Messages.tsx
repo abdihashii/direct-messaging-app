65a052f380da441de6e412f83cc89c8e40cc242e
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-28 20:08:29 -0500

    should be same as in prod

--- client/app/chat/[chat_id]/Messages.tsx
+++ client/app/chat/[chat_id]/Messages.tsx
@@ -1,36 +1,32 @@
 'use client';
 
-import { ScrollArea } from '@/components/ui/scroll-area';
 import { Message } from '@/types';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useRouter } from 'next/navigation';
-import { useRef, useEffect } from 'react';
-
-const AlwaysScrollToBottom = () => {
-  const elementRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => elementRef.current?.scrollIntoView());
-
-  return <div ref={elementRef} />;
-};
+import { useRef, useEffect, useState } from 'react';
 
 const Messages = ({
-  messages,
+  messages: serverMessages,
   userId,
 }: {
   messages: Message[];
   userId: string;
 }) => {
-  // const chatContainerRef = useRef<HTMLDivElement>(null);
+  const chatContainerRef = useRef<HTMLDivElement>(null);
   const supabase = createClientComponentClient();
   const router = useRouter();
+  const [messages, setMessages] = useState<Message[]>(serverMessages);
+
+  useEffect(() => {
+    setMessages(serverMessages);
+  }, [serverMessages]);
 
   useEffect(() => {
     // Scroll to bottom
-    // if (chatContainerRef.current) {
-    //   chatContainerRef.current.scrollTop =
-    //     chatContainerRef.current.scrollHeight;
-    // }
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
 
     // Listen to inserts
     const channel = supabase
@@ -38,48 +34,46 @@ const Messages = ({
       .on(
         'postgres_changes',
         { event: 'INSERT', schema: 'public', table: 'messages' },
-        () => {
-          router.refresh();
+        (payload) => {
+          setMessages([...messages, payload.new as Message]);
         },
       )
       .subscribe();
 
     return () => {
-      channel.unsubscribe();
+      supabase.removeChannel(channel);
     };
   }, [messages, supabase, router]);
 
   return (
-    <ScrollArea className="p-5">
-      <div className="flex flex-col items-center gap-8">
-        <ul className="flex w-full flex-col gap-4">
-          {messages?.map((m) => {
-            const iAmTheSender = userId === m.sender_id;
+    <div
+      ref={chatContainerRef}
+      className="flex flex-col items-center gap-8 overflow-y-scroll p-5"
+    >
+      <ul className="flex w-full flex-col gap-4">
+        {messages?.map((m) => {
+          const iAmTheSender = userId === m.sender_id;
 
-            return (
-              <li
-                className={`flex max-w-[50%] flex-col gap-2 rounded-md border border-gray-500 p-2 ${
-                  iAmTheSender
-                    ? 'ml-auto items-end'
-                    : 'items-start justify-start'
+          return (
+            <li
+              className={`flex max-w-[50%] flex-col gap-2 rounded-md border border-gray-500 p-2 ${
+                iAmTheSender ? 'ml-auto items-end' : 'items-start justify-start'
+              }`}
+              key={m.message_id}
+            >
+              <p
+                className={`text-sm ${
+                  iAmTheSender ? 'text-green-700' : 'text-blue-700'
                 }`}
-                key={m.message_id}
               >
-                <p
-                  className={`text-sm ${
-                    iAmTheSender ? 'text-green-700' : 'text-blue-700'
-                  }`}
-                >
-                  {m.sender_user_name}
-                </p>
-                <p>{m.content}</p>
-              </li>
-            );
-          })}
-          <AlwaysScrollToBottom />
-        </ul>
-      </div>
-    </ScrollArea>
+                {m.sender_user_name}
+              </p>
+              <p>{m.content}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
