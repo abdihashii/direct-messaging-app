6a76030846c548caeef3fca92bc087761087806e
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-28 20:10:24 -0500

    Fixes loading pages

--- client/app/chat/loading.tsx
+++ client/app/chat/loading.tsx
@@ -1,7 +1,7 @@
 export default function LoadingChatRooms() {
   return (
     <section className="flex flex-col h-full justify-center items-center gap-4 p-8">
-      <p className="text-center text-xl">Loading chats...</p>
+      <p className="text-center text-xl">Loading Messages...</p>
     </section>
   );
 }
