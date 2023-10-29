beea7177901b5cd6d9a5ea2a04e79b044ed07b23
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-28 21:46:42 -0500

    Shows the chat name in the chat!

--- client/app/chat/[chat_id]/layout.tsx
+++ client/app/chat/[chat_id]/layout.tsx
@@ -1,10 +1,12 @@
-import { getUser } from '@/lib/supabaseServerClient';
+import {
+  createServerSupabaseClient,
+  getUser,
+} from '@/lib/supabaseServerClient';
 import SignOut from '../../auth/SignOut';
 import { redirect } from 'next/navigation';
 import { ChevronLeft } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import { getChatName } from '@/lib/utils';
 
 export default async function ChatLayout({
   params,
@@ -15,6 +17,7 @@ export default async function ChatLayout({
   };
   children: React.ReactNode;
 }) {
+  const supabase = createServerSupabaseClient();
   const user = await getUser();
 
   // If there is no user, redirect to the sign in page
@@ -22,10 +25,24 @@ export default async function ChatLayout({
     redirect('/auth/sign-in');
   }
 
+  const getChatName = async (chat_id: string) => {
+    if (!chat_id) return '';
+    try {
+      const { data, error } = await supabase
+        .from('chats')
+        .select('chat_name')
+        .eq('chat_id', chat_id)
+        .single();
+
+      if (error) throw error;
+      return data.chat_name || '';
+    } catch (error) {
+      return '';
+    }
+  };
+
   // Gets the chat name
   const chatName = await getChatName(params.chat_id);
-
-  console.log(chatName);
 
   return (
     <div
@@ -34,16 +51,14 @@ export default async function ChatLayout({
         gridTemplateRows: 'auto 1fr',
       }}
     >
-      <header className="h-24 flex flex-row justify-between items-center gap-2 border-b-2 border-b-gray-500 p-8">
+      <header className="flex h-24 flex-row items-center justify-between gap-2 border-b-2 border-b-gray-500 p-8">
         <Link href="/chats">
           <Button size={'icon'}>
             <ChevronLeft />
           </Button>
         </Link>
 
-        <h1 className="text-xl text-center">{'group chat name goes here'}</h1>
-
-        {/* <p className="ml-auto">Settings</p> */}
+        <h1 className="text-center text-xl">{chatName}</h1>
 
         {user && <SignOut />}
       </header>
