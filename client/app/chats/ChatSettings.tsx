b1cf977eb831fca8097623ef2f3c7bd24d9591c1
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-10-30 22:53:34 -0500

    Basic functionality for searching for users

--- client/app/chats/ChatSettings.tsx
+++ client/app/chats/ChatSettings.tsx
@@ -4,9 +4,30 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Ban, Loader2, Pencil, Save } from 'lucide-react';
-import { useState } from 'react';
+import {
+  Ban,
+  // Check,
+  Loader2,
+  Pencil,
+  Save,
+} from 'lucide-react';
+import { useEffect, useState } from 'react';
 import { useToast } from '@/components/ui/use-toast';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from '@/components/ui/command';
+// import { cn } from '@/lib/utils';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import type { Database } from '@/types/database.types';
+import type { Profile } from '@/types';
 
 const ChatSettings = ({
   chat,
@@ -21,8 +42,12 @@ const ChatSettings = ({
   const [editChatName, setEditChatName] = useState(false);
   const [isChatNameLoading, setIsChatNameLoading] = useState(false);
   const [chatName, setChatName] = useState(chat.chat_name as string);
+  const [open, setOpen] = useState(false);
+  const [userValues, setUserValues] = useState<string[]>([]);
+  const [userProfiles, setUserProfiles] = useState<Profile[]>([]);
+  const [isUserProfilesLoading, setIsUserProfilesLoading] = useState(false);
   const { toast } = useToast();
-  const supabase = createClientComponentClient();
+  const supabase = createClientComponentClient<Database>();
 
   const handleChatNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setChatName(e.target.value);
@@ -51,6 +76,43 @@ const ChatSettings = ({
       });
     }
   };
+
+  const getUserProfiles = async () => {
+    setIsUserProfilesLoading(true);
+
+    try {
+      const { data: profiles, error } = await supabase
+        .from('profiles')
+        .select('*');
+
+      if (error) throw error;
+
+      setUserProfiles(profiles);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsUserProfilesLoading(false);
+    }
+  };
+
+  const handleOnUserNameSelect = (currentValue: string) => {
+    // check if user_name is already in userValues
+    if (userValues.includes(currentValue)) {
+      toast({
+        description: 'User already added.',
+        duration: 1500,
+      });
+    } else {
+      setUserValues([...userValues, currentValue]);
+    }
+
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    getUserProfiles();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
@@ -98,6 +160,52 @@ const ChatSettings = ({
           </section>
         )}
       </form>
+
+      <section className="flex w-full flex-col gap-2">
+        <Label>Add People</Label>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Input
+              type="text"
+              placeholder="Find and add people to your chat..."
+              value={userValues as string[]}
+              readOnly={true}
+            />
+          </PopoverTrigger>
+
+          <PopoverContent className="w-72 p-0">
+            <Command>
+              <CommandInput placeholder="Search users..." />
+              <CommandEmpty>User not found.</CommandEmpty>
+              <CommandGroup>
+                {userProfiles.map((profile) => (
+                  <CommandItem
+                    key={profile.user_id}
+                    value={profile.user_name as string}
+                    onSelect={(currentValue) => {
+                      handleOnUserNameSelect(currentValue);
+                    }}
+                  >
+                    {/* <Check
+                      className={cn(
+                        'mr-2 h-4 w-4',
+                        userValue === profile.user_name
+                          ? 'opacity-100'
+                          : 'opacity-0',
+                      )}
+                    /> */}
+                    {profile.user_name}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+        </Popover>
+      </section>
+
+      <section className="w-full overflow-x-scroll overflow-y-scroll">
+        <pre>{JSON.stringify(userValues, null, 2)}</pre>
+      </section>
     </>
   );
 };
