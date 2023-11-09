55a26ba7c53e52204d2d589773feb07a91f1895b
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-11-09 14:58:57 -0600

    adds loading skeletons for chat and messages lists

--- client/app/chat/[chat_id]/loading.tsx
+++ client/app/chat/[chat_id]/loading.tsx
@@ -1,7 +1,12 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
 export default function LoadingChatRooms() {
   return (
-    <section className="flex flex-col h-full justify-center items-center gap-4 p-8">
-      <p className="text-center text-xl">Loading Messages...</p>
+    <section className="flex h-full flex-col items-center justify-center gap-4 p-8">
+      <Skeleton className="ml-auto flex h-20 w-[60%] flex-col gap-2 rounded-md border border-gray-500 p-2" />
+      <Skeleton className="flex h-20 w-[60%] flex-col items-start gap-2 rounded-md border border-gray-500 p-2" />
+      <Skeleton className="ml-auto flex h-20 w-[60%] flex-col gap-2 rounded-md border border-gray-500 p-2" />
+      <Skeleton className="flex h-20 w-[60%] flex-col items-start gap-2 rounded-md border border-gray-500 p-2" />
     </section>
   );
 }
--- client/app/chats/loading.tsx
+++ client/app/chats/loading.tsx
@@ -1,7 +1,13 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
 export default function LoadingChatRooms() {
   return (
-    <section className="flex flex-col h-full justify-center items-center gap-4 p-8">
-      <p className="text-center text-xl">Loading chats...</p>
-    </section>
+    <div className="flex h-full flex-col py-10">
+      <section className="flex flex-col gap-4">
+        <Skeleton className="mx-auto flex h-[3.625rem] w-10/12 flex-col gap-2 rounded-md border border-black" />
+        <Skeleton className="mx-auto flex h-[3.625rem] w-10/12 flex-col gap-2 rounded-md border border-black" />
+        <Skeleton className="mx-auto flex h-[3.625rem] w-10/12 flex-col gap-2 rounded-md border border-black" />
+      </section>
+    </div>
   );
 }
